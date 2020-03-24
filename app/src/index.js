eb186eccc76fbbfa8ad12b508ffd71c608f63b58
SadCat88 <SadCat88@yandex.ru>
2020-03-24 07:57:32 +0300

    Меню.

--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,25 @@
 'use strict';
 
 console.log('index.js is run');
+
+const leftButton = $('#leftButton');
+console.log('leftButton', leftButton);
+
+let click = 0;
+
+$('#leftButton').mouseover(function(event) {
+  $('.navigation').addClass('active');
+});
+
+$('#leftButton').click(function(event) {
+  if (click == 0) {
+    $('.navigation').addClass('active');
+  } else {
+    $('.navigation').toggleClass('active');
+  }
+  click += 1;
+});
+
+$('#navigation').mouseleave(function(event) {
+  $('.navigation').removeClass('active');
+});
